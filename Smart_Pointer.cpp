8ddb2f319257756c30de9f3744659337729d7103
NakulChauhan1 <nakulchauhan111@gmail.com>
2024-12-14 10:47:18 +0530

    temp commit for SmartPointers

--- Smart_Pointer.cpp
+++ Smart_Pointer.cpp
@@ -24,13 +24,19 @@ void print(int* ptr, int size){
     std::cout<<std::endl;
 }
 
+void print(std::shared_ptr<int[]> sptr, int size){
+    std::cout<<"Reference count (inside print fxn ): "<<sptr.use_count()<<std::endl;
+    for(int i=0;i<size;i++){
+        std::cout<<" sptr[i] = "<<sptr[i]<<" ";
+    }
+    std::cout<<std::endl;
+}
+
 int main(){
     std::cout<<"Smart Pointers"<<std::endl;
 
-    int var=10;
-
-    int* ptr=&var;                           //using raw pointer
-    std::unique_ptr<int> uptr( &var );       //using smart pointer
+    int* ptr=new int{55};                           //using raw pointer
+    std::unique_ptr<int> uptr( ptr );       //using smart pointer
 
     std::cout<<"var="<<*ptr<<std::endl;
     std::cout<<"var="<<*uptr<<std::endl;
@@ -52,9 +58,6 @@ int main(){
     std::unique_ptr<int> uptr4 = std::move(uptr);              //not an error: because copy is not allowed with unique pointer, but moving of a resource is allowed
     // std::cout<<"var="<<*uptr<<std::endl;                    //runtime error: because uptr underlying resource (ie int here) is moved to uptr4
                                                                //note: we cannot use objects like unique pointer once they are moved
-    
-    
-
 
 
     std::shared_ptr<int> sharedptr1( new int(1000) );
@@ -64,10 +67,48 @@ int main(){
     std::cout<<"sharedptr2: "<<*sharedptr2<<std::endl;
     std::cout<<"sharedptr1: "<<*sharedptr1<<std::endl;        //can access even after assigning to some other pointer
 
-    TODOOOO:
+    // TODOOOO:
     // std::vector<int> array{1,2,3,4,5};
     // std::unique_ptr<std::vector<int>> sptr2(array.data());
 
+    std::cout<<"******************************************** Weak Pointers *************************************************************************"<<std::endl;
+    std::shared_ptr<int[]> sharedptr3{ new int[5]{1,2,3,4,5} };
+    std::shared_ptr<int[]> sharedptr4;
+    sharedptr4 = sharedptr3;
+
+    print(sharedptr3, 5);
+    print(sharedptr4, 5);
+
+    std::cout<<"Reference count, sharedptr3: "<<sharedptr3.use_count()<<std::endl;
+    std::cout<<"Reference count, sharedptr4: "<<sharedptr4.use_count()<<std::endl;
+
+    sharedptr3 = NULL;                  //make pointer NULL to decrement reference count, its like freeing memory, but in reality memory is freed after object will go out of scope
+
+    std::cout<<"Reference count, sharedptr3: "<<sharedptr3.use_count()<<std::endl;
+    std::cout<<"Reference count, sharedptr4: "<<sharedptr4.use_count()<<std::endl;
+
+    std::cout<<std::endl<<"after freeing sharedptr3"<<std::endl;
+    
+    print(sharedptr4, 5);
+    // print(sharedptr3, 5);
+
+    //note: above when one object is released then other object which was assigned from it still points to that underlying resource, this may be not wanted in some situations.
+    /* in some places, we would want if original object is freed then other object which were initialized from it should also get released. We would want some kind of communication
+       between these objects. (from there comes the need of new shared pointer called as weak_pointer) */
+
+
+    std::shared_ptr<int> sharedptr5 {new int { 97800 }};
+    std::weak_ptr<int> wkptr {sharedptr5};
+
+    std::cout<<"Printing information via weak ptr"<<std::endl;
+
+    if (std::shared_ptr<int> sptrrrr = wkptr.lock()) { // Obtain a shared_ptr from the weak_ptr
+        std::cout << "Weak ptr: " << *sptrrrr << std::endl;
+    } else {
+        std::cout << "The managed object has been deleted." << std::endl;
+    }
+
+    std::cout<<"Weak ptr: "<<*wkptr<<std::endl;
 
 
     return 0;
